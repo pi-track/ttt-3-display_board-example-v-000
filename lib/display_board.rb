0a8e540dcb5d9e4fa05943a8865815250032279d
Patrick Eells <patrick.eells@gmail.com>
2018-01-02 19:52:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-puts "
-   |   |
------------
-   |   |
------------
-   |   |
-   "
+def display_board
+  puts "
+     |   |
+     -----------
+        |   |
+        -----------
+           |   |
+           "
